5ee3b7dfb9a5df1f71328248cd5c67819f12f520
Anny85-code <aniekaudo@yahoo.com>
2023-02-10 12:12:20 -0800

    Add file no 15

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,57 +1,45 @@
 #!/usr/bin/python3
-""" Module to print status code """
-import sys
 
+def read_input_and_compute_metrics():
+    """
+    Reads input from stdin line by line and computes metrics.
 
-class Magic:
-    """ Class to generates instances with dict and size"""
-    def __init__(self):
-        """ Init method """
-        self.dic = {}
-        self.size = 0
+    Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+    Each 10 lines and after a keyboard interruption (CTRL + C), prints those statistics since the beginning:
+    Total file size: File size: <total size>
+    where is the sum of all previous (see input format above)
+    Number of lines by status code:
+    possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+    if a status code doesn’t appear, don’t print anything for this status code
+    format: <status code>: <number>
+    status codes should be printed in ascending order
+    """
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    total_size = 0
+    line_count = 0
 
-    def init_dic(self):
-        """ Initialize dict """
-        self.dic['200'] = 0
-        self.dic['301'] = 0
-        self.dic['400'] = 0
-        self.dic['401'] = 0
-        self.dic['403'] = 0
-        self.dic['404'] = 0
-        self.dic['405'] = 0
-        self.dic['500'] = 0
+    try:
+        while True:
+            line = input().strip()
+            fields = line.split()
+            status_code = int(fields[-2])
+            file_size = int(fields[-1])
+            total_size += file_size
+            status_codes[status_code] += 1
+            line_count += 1
 
-    def add_status_code(self, status):
-        """ add repeated number to the status code """
-        if status in self.dic:
-            self.dic[status] += 1
-
-    def print_info(self, sig=0, frame=0):
-        """ print status code """
-        print("File size: {:d}".format(self.size))
-        for key in sorted(self.dic.keys()):
-            if self.dic[key] is not 0:
-                print("{}: {:d}".format(key, self.dic[key]))
+            if line_count % 10 == 0:
+                print("Total file size: File size: {}".format(total_size))
+                for code in sorted(status_codes.keys()):
+                    if status_codes[code] > 0:
+                        print("{}: {}".format(code, status_codes[code]))
+    except KeyboardInterrupt:
+        print("Total file size: File size: {}".format(total_size))
+        for code in sorted(status_codes.keys()):
+            if status_codes[code] > 0:
+                print("{}: {}".format(code, status_codes[code]))
 
 
 if __name__ == "__main__":
-    magic = Magic()
-    magic.init_dic()
-    nlines = 0
+    read_input_and_compute_metrics()
 
-    try:
-        for line in sys.stdin:
-            if nlines % 10 == 0 and nlines is not 0:
-                magic.print_info()
-
-            try:
-                list_line = [x for x in line.split(" ") if x.strip()]
-                magic.add_status_code(list_line[-2])
-                magic.size += int(list_line[-1].strip("\n"))
-            except:
-                pass
-            nlines += 1
-    except KeyboardInterrupt:
-        magic.print_info()
-        raise
-    magic.print_info()
